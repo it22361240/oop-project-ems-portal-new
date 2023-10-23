966d5c1c0d91730382e92f6d9c3049a63358db62
Shanelka Pramuditha <99pramuditha@gmail.com>
2023-10-23 09:05:15 +0530

    role based login added

--- src/main/java/com/ems/user/loginServlet.java
+++ src/main/java/com/ems/user/loginServlet.java
@@ -6,9 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import com.ems.db.DatabaseConnection;
@@ -22,57 +25,59 @@ import com.ems.db.DatabaseConnection;
 public class  loginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
-    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		PrintWriter out = response.getWriter();
-//		out.print("working....    ");
-
-		
 		
 		try {
 			// Establish a database connection
             Connection conn = DatabaseConnection.getConnection();
 
-			java.sql.PreparedStatement pst = conn.prepareStatement("SELECT * FROM ems.users where username = ? and password = ?;");
+			PreparedStatement pst = conn.prepareStatement("SELECT * FROM ems.users where username = ? and password = ?;");
 			pst.setString(1, request.getParameter("username"));
 			pst.setString(2, request.getParameter("password"));
 			ResultSet row = pst.executeQuery();
 			if (row.next()) {
-//				PrintWriter out = response.getWriter();
-//				out.print("working....    ");
-				
-				
+
 				// this is create the another jsp file and send data to this file
-				String id = row.getString("id");
+				String uId = row.getString("uId");
 				String username = row.getString("username");
 			    String password = row.getString("password");
 			    String name = row.getString("uName");
 			    String email = row.getString("email");
 			    int phone = row.getInt("phone");
-			    System.out.println(id);
+			    String role = row.getString("role");
+			    
+			    HttpSession session = request.getSession();
+			    session.setAttribute("uId", uId);
+			    session.setAttribute("role", role);
 				
-				RequestDispatcher dip = request.getRequestDispatcher("./views/users/profile.jsp");
-				request.setAttribute("id", id);
-				request.setAttribute("username", username);
-				request.setAttribute("password", password);	
-				request.setAttribute("name", name);
-				request.setAttribute("email", email);	
-				request.setAttribute("phone", phone);	
-				dip.forward(request, response);
+			    if (role.equals("student")) {
+					RequestDispatcher dip = request.getRequestDispatcher("./views/users/dash/sDashboard.jsp");
+					request.setAttribute("uId", uId);
+					request.setAttribute("username", username);
+					request.setAttribute("password", password);	
+					request.setAttribute("name", name);
+					request.setAttribute("email", email);	
+					request.setAttribute("phone", phone);
+					request.setAttribute("role", role);
+					dip.forward(request, response);
+			    }
+			    else if (role.equals("teacher")) {
+			    	RequestDispatcher dip = request.getRequestDispatcher("./views/users/dash/tDashboard.jsp");
+			    	dip.forward(request, response);
+			    }
+			    else if (role.equals("teacher")) {
+			    	RequestDispatcher dip = request.getRequestDispatcher("./views/users/dash/aDashboard.jsp");
+			    	dip.forward(request, response);
+			    }
 
 			}else {
 				System.out.println(Integer.parseInt(request.getParameter("password")));
-				
 			}
 			
 			// Close the database connection
             conn.close();
 		}catch(Exception e){
-			PrintWriter out = response.getWriter();
-			out.print("not working....    ");
 			e.printStackTrace();
-			System.out.println(e);
-			
 		}
 	}
 
--- src/main/webapp/views/users/profile.jsp
+++ src/main/webapp/views/users/profile.jsp
@@ -10,13 +10,14 @@
 </head>
 <body>
 <h1>Login successfully</h1>
-<p> Recieved Id : ${id }</p>
+<p> Recieved Id : ${uId }</p>
 <h1>Login successfully</h1>
    <p>Received username: ${username}</p>
     <p>Received password: ${password}</p>
     <p>Received email: ${email}</p>
     <p>Received name: ${name}</p>
     <p>Received phone: ${phone}</p>
+    <p>Role: ${role}</p>
     <c:url value="./views/users/updateUser.jsp" var="update">
     	<c:param name = "id" value = "${username}"/>
     </c:url>
@@ -28,14 +29,14 @@
     <script>
     document.getElementById("update").addEventListener("click", function() {
         // Define the values you want to send to the new JSP
-        var id = "${id}";
+        var id = "${uId}";
         var username = "${username}";
         var password = "${password}";
         var email = "${email}";
         var name = "${name}";
         var phone = "${phone}";
         // Construct the URL with parameters
-        var url = "./views/users/updateUser.jsp?id=" + id + "&password=" + password+ "&email=" + email+ "&name=" + name+ "&phone=" + phone+ "&username=" + username;
+        var url = "./views/users/updateUser.jsp?id=" + uId + "&password=" + password+ "&email=" + email+ "&name=" + name+ "&phone=" + phone+ "&username=" + username;
         // Redirect to the new JSP
         window.location.href = url;
     });
@@ -49,7 +50,7 @@
         var name = "${name}";
         var phone = "${phone}";
         // Construct the URL with parameters
-        var url = "./views/users/confirmDelete.jsp?id=" + id + "&password=" + password+ "&email=" + email+ "&name=" + name+ "&phone=" + phone+ "&username=" + username;
+        var url = "./views/users/confirmDelete.jsp?id=" + uId + "&password=" + password+ "&email=" + email+ "&name=" + name+ "&phone=" + phone+ "&username=" + username;
         // Redirect to the new JSP
         window.location.href = url;
     });
